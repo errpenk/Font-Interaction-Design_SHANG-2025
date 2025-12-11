00912bf349ee9fcc09784911189ff6d43d7e35d6
errpenk <errpenk@gmail.com>
2025-12-11 19:19:22 +0100

    Create typography.js

---
+++ src/config/typography.js
@@ -0,0 +1,14 @@
+/*
+
+export const GLYPHS_LOWER = {
+  a: '<path d="M20 45 H80 V90 H20 V65 H80" />',
+  b: '<path d="M20 10 V90 H80 V55 H20" />',
+  c: '<path d="M80 45 H20 V90 H80" />',
+  d: '<path d="M80 10 V90 H20 V55 H80" />',
+  // ... (keep data layer) ...
+  z: '<path d="M20 45 H80 L20 90 H80" />',
+};
+
+export const GLYPHS_UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+
+*/
